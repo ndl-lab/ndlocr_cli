7d9a209f3ea2fffbb1d9ea8d491d0e029202d261
ndl-lab <54262546+ndl-lab-staff@users.noreply.github.com>
2022-04-04 17:15:27 +0900

    Update dockerbuild.sh

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -1,6 +1,10 @@
 TAG=ocr-cli-py37
 DOCKERIGNORE=docker/dockerignore
 DOCKERFILE=docker/Dockerfile
+wget https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/mojilist_NDL.txt -P ./src/text_recognition/models
+wget https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/ndlenfixed64-mj0-synth1.pth -P ./src/text_recognition/models
+wget https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/ndl_layout_config.py -P ./src/ndl_layout/models
+wget https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/epoch_140_all_eql_bt.pth -P ./src/ndl_layout/models
 
 cp ${DOCKERIGNORE} .dockerignore
 docker build -t ${TAG} -f ${DOCKERFILE} .
