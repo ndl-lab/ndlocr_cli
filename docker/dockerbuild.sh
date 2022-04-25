f3be41f3281f25efe3d0a52b2a989bb44e32ae5a
ndl-lab-staff <lab@ndl.go.jp>
2022-04-25 17:10:04 +0900

    インストール関連を微修正

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -5,6 +5,7 @@ wget https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/mojilist_NDL.txt -P .
 wget https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/ndlenfixed64-mj0-synth1.pth -P ./src/text_recognition/models
 wget https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/ndl_layout_config.py -P ./src/ndl_layout/models
 wget https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/epoch_140_all_eql_bt.pth -P ./src/ndl_layout/models
+wget https://lab.ndl.go.jp/dataset/ndlocr/separate_pages_ssd/weights.hdf5 -P ./src/separate_pages_ssd/ssd_tools
 
 cp ${DOCKERIGNORE} .dockerignore
 docker build -t ${TAG} -f ${DOCKERFILE} .
