6c62e54f8ba8ea182ccdb18a7ad65812345da02f
ndl-lab <54262546+ndl-lab-staff@users.noreply.github.com>
2022-05-09 09:29:17 +0900

    Update dockerbuild.sh

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -1,11 +1,11 @@
 TAG=ocr-cli-py37
 DOCKERIGNORE=docker/dockerignore
 DOCKERFILE=docker/Dockerfile
-wget https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/mojilist_NDL.txt -P ./src/text_recognition/models
-wget https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/ndlenfixed64-mj0-synth1.pth -P ./src/text_recognition/models
-wget https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/ndl_layout_config.py -P ./src/ndl_layout/models
-wget https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/epoch_140_all_eql_bt.pth -P ./src/ndl_layout/models
-wget https://lab.ndl.go.jp/dataset/ndlocr/separate_pages_ssd/weights.hdf5 -P ./src/separate_pages_ssd/ssd_tools
+wget -nc https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/mojilist_NDL.txt -P ./src/text_recognition/models
+wget -nc https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/ndlenfixed64-mj0-synth1.pth -P ./src/text_recognition/models
+wget -nc https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/ndl_layout_config.py -P ./src/ndl_layout/models
+wget -nc https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/epoch_140_all_eql_bt.pth -P ./src/ndl_layout/models
+wget -nc https://lab.ndl.go.jp/dataset/ndlocr/separate_pages_ssd/weights.hdf5 -P ./src/separate_pages_ssd/ssd_tools
 
 cp ${DOCKERIGNORE} .dockerignore
 docker build -t ${TAG} -f ${DOCKERFILE} .
