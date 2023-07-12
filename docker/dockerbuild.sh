384b4c582834e90cd924443f00238aa427bbbb32
ndl-lab <54262546+ndl-lab-staff@users.noreply.github.com>
2023-07-12 12:51:24 +0900

    Update dockerbuild.sh

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -3,7 +3,6 @@ DOCKERIGNORE=docker/dockerignore
 DOCKERFILE=docker/Dockerfile
 wget -nc https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/mojilist_NDL.txt -P ./src/text_recognition/models
 wget -nc https://lab.ndl.go.jp/dataset/ndlocr/text_recognition/ndlenfixed64-mj0-synth1.pth -P ./src/text_recognition/models
-wget -nc https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/ndl_layout_config.py -P ./src/ndl_layout/models
 wget -nc https://lab.ndl.go.jp/dataset/ndlocr/ndl_layout/epoch_140_all_eql_bt.pth -P ./src/ndl_layout/models
 wget -nc https://lab.ndl.go.jp/dataset/ndlocr/separate_pages_ssd/weights.hdf5 -P ./src/separate_pages_ssd/ssd_tools
 
