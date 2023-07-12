46ec0305484d28a6dc775c44100791faff70d1e4
ndl-lab <54262546+ndl-lab-staff@users.noreply.github.com>
2023-07-12 10:24:44 +0900

    Update dockerbuild.sh

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -4,7 +4,7 @@ DOCKERFILE=docker/Dockerfile
 wget -nc https://lab.ndl.go.jp/dataset/ndlocr_v2/text_recognition_lightning/resnet-orient2.ckpt -P ./submodules/text_recognition_lightning/models
 wget -nc https://lab.ndl.go.jp/dataset/ndlocr_v2/text_recognition_lightning/rf_author/model.pkl -P ./submodules/text_recognition_lightning/models/rf_author/
 wget -nc https://lab.ndl.go.jp/dataset/ndlocr_v2/text_recognition_lightning/rf_title/model.pkl -P ./submodules/text_recognition_lightning/models/rf_title/
-wget -nc https://lab.ndl.go.jp/dataset/ndlocr_v2/ndl_layout/ndl_retrainmodel.pth -P ./submodules/ndl_layout/models
+wget -nc https://lab.ndl.go.jp/dataset/ndlocr_v2/ndl_layout/epoch_375.pth -P ./submodules/ndl_layout/models
 wget -nc https://lab.ndl.go.jp/dataset/ndlocr_v2/separate_pages_mmdet/epoch_180.pth -P ./submodules/separate_pages_mmdet/models
 cp ${DOCKERIGNORE} .dockerignore
 docker build -t ${TAG} -f ${DOCKERFILE} .
